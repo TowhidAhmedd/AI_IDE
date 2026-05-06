06fd896731904c4c77f4bbef17093112418823e3
Towhid Ahmed <121598055+TowhidAhmedd@users.noreply.github.com>
2026-05-06 07:49:46 +0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://ai-ide-frontend.onrender.com"
+        "https://ai-ide-frontend2.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
